f6931dc81186fec1c9156e5e0fce499eb90f1f98
Marco Magdy <mmagdy@gmail.com>
2019-10-07 22:54:34 +0000

    Remove AppMesh specific env. variable
    Signed-off-by: Marco Magdy <mmagdy@gmail.com>

--- source/extensions/tracers/xray/xray_tracer_impl.cc
+++ source/extensions/tracers/xray/xray_tracer_impl.cc
@@ -13,24 +13,8 @@ Driver::Driver(const XRayConfiguration& config, Server::Instance& server) : xray
       config.daemon_endpoint_.empty() ? DEFAULT_DAEMON_ENDPOINT : config.daemon_endpoint_;
 
   ENVOY_LOG(debug, "send X-Ray generated segments to daemon address on {}", daemon_endpoint);
-  std::string span_name = server.localInfo().clusterName();
-  // XRay segment name will be the combination of mesh name and the virtual node name.
-  const auto vnode_name = std::getenv(ENV_APPMESH_NODE_NAME);
-  if (vnode_name) {
-    // should be in the following format:
-    // mesh/{meshName}/virtualNode/{virtualNodeName}.
-    const auto parts = StringUtil::splitToken(vnode_name, "/", false /*keep_empty_string*/);
-    if (parts.size() >= 4) {
-      span_name = absl::StrCat(parts[1], "/", parts[3]);
-    } else {
-      ENVOY_LOG(debug,
-                "Failed to construct an XRay span name from env. variable {} - using cluster's "
-                "name instead",
-                vnode_name);
-    }
-  } else if (!config.segment_name_.empty()) {
-    span_name = config.segment_name_;
-  }
+  std::string span_name =
+      config.segment_name_.empty() ? server.localInfo().clusterName() : config.segment_name_;
 
   sampling_strategy_ = std::make_unique<XRay::SamplingStrategy>(server.random().random());
   tracer_.emplace(span_name, server.timeSource());
